a659200cad21ab653b36e663b411dd3ded297d16
Alvaro Suarez <varosuarez@gmail.com>
2020-02-01 17:26:42 +0100

    destroyable comp done

--- Assets/Scripts/StickyFloor.cs
+++ Assets/Scripts/StickyFloor.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class StickyFloor : MonoBehaviour
 {
-    private Vector3 m_EnterScale = Vector3.one;
-    public Transform m_globalParent = null; 
-    public Transform m_transformToAttach;
-
-    void Start()
-    {
-        if (m_transformToAttach == null)
-            m_transformToAttach = transform;
-    }
 
     void OnCollisionEnter2D(Collision2D other)
     {
