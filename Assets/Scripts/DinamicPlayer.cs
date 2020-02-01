bab7f8a4678ba32c3cb9f0069fb0d1174b96c685
Alvaro Suarez <varosuarez@gmail.com>
2020-02-01 22:58:37 +0100

    input

--- Assets/Scripts/DinamicPlayer.cs
+++ Assets/Scripts/DinamicPlayer.cs
@@ -58,6 +58,8 @@ public class DinamicPlayer : MonoBehaviour, InputMaster.IPlayerActions
     private GameObject objectToCatch;
     private bool availableCatch;
 
+    public Transform carryingPos;
+
     private void Awake() {
         inputMaster = new InputMaster();
         inputMaster.Player.SetCallbacks(this);
---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUp : MonoBehaviour
+{
+
+    private Transform taker;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            transform.parent = taker;
+            //this.transform.position = GameObject.FindGameObjectWithTag("Player").GetComponent<DinamicPlayer>().carryingPos.position;
+        }
+        else if (Input.GetKeyUp(KeyCode.E))
+        {
+            
+            transform.parent = null;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.CompareTag("Player"))
+        {
+            Debug.Log("E");
+            taker = col.GetComponent<DinamicPlayer>().carryingPos;
+            Destroy(GetComponent<Rigidbody2D>());
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+
+        if (col.CompareTag("Player"))
+        {
+            Debug.Log("Ew");
+            taker = null;
+            gameObject.AddComponent<Rigidbody2D>();
+        }
+
+    }
+}
