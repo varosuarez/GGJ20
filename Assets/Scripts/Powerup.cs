46c063d7d12ea688632bd7159dda057e24eff0c2
JorgeAriasLopez <ariasljorge@gmail.com>
2020-02-02 06:27:35 +0100

    powerup sound

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -4,11 +4,18 @@ public class Powerup : MonoBehaviour
 {
     [SerializeField]
     private DinamicPlayer.State power = DinamicPlayer.State.Powerless;
+    public AudioClip m_powerupSound;
 
     private void OnTriggerEnter2D(Collider2D col) {
         if (col.CompareTag("Player")) {
             col.GetComponent<DinamicPlayer>().SetState(power);
             col.GetComponent<DinamicPlayer>().DiscoverUI(power);
+            if (m_powerupSound != null)
+            {
+                AudioSource m_audio = GameObject.FindGameObjectWithTag("Player").GetComponent<AudioSource>();
+                m_audio.clip = m_powerupSound;
+                m_audio.Play();
+            }
             Destroy(gameObject);
         }
     }
